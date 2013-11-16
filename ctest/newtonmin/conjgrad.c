1d95bf2c4cee4ae05bc3dc8059ca0e4463189624
Rafat Hussain <rafat.hsn@gmail.com>
2013-11-16 14:29:26 +0530

    Commit : Added conjgrad.c

--- ctest/newtonmin/conjgrad.c
+++ ctest/newtonmin/conjgrad.c
@@ -1,1 +1,73 @@
 #include "conjgrad.h"
+
+static int richolu(double *A,int N, int stride, double *U22) {
+	int sc;
+	int j,i,u,w;
+	double u11;
+	
+	if (N == 1) {
+		if (A[0] > 0) {
+			A[0] = sqrt(A[0]);
+			return 0;
+		} else {
+			return -1;
+		}
+	} else {
+		if (A[0] < 0) {
+			return -1;
+		}
+		u11 = sqrt(A[0]);
+		A[0] = u11;
+		for (j = 1; j < N;++j) {
+			if (A[j] != 0) {
+				A[j] /= u11;
+			}
+		}
+		mmult(A+1,A+1,U22,N-1,1,N-1);
+		for (i = 0; i < N-1; ++i) {
+			u = stride + 1 + i * stride;
+			w = i * (N-1);
+			for(j = i; j < N-1;j++) {
+				if (A[j + u] != 0) {
+					A[j + u] -= U22[j + w];
+				}
+			}
+		}
+		
+		sc = richolu(A+stride+1,N-1,stride,U22);
+		if (sc == -1) {
+			return -1;
+		}
+		
+	}
+	
+	return sc;
+	
+}
+
+
+int icholu(double *A, int N) {
+	int stride,i,j,t,sc;
+	double *U22;
+	U22 = (double*) malloc(sizeof(double) * N * N);
+	stride = N; 
+	
+	sc = richolu(A,N,stride,U22);
+	
+	for(i=0; i < N;++i) {
+		t = i *N;
+		for(j=0;j < i;++j) {
+			A[t+j] = 0.;
+		}
+	}
+
+	free(U22);
+	return sc;
+	
+}
+
+int ichol(double *A, int N) {
+	int sc;
+	sc = icholu(A,N);
+	return sc;
+}
